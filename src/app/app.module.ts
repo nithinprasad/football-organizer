e2f90678e961fbf6cf269a95f4191080c12fbdc5
Nithin Prasad <nithinprasad549@yahoo.com>
2023-11-26 00:07:40 +0530

    Firestore fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 import { SocialmediabannerComponent } from './component/socialmediabanner/socialmediabanner.component';
 import { MenuComponent } from './component/menu/menu.component';
 import { MainCarouselComponent } from './component/main-carousel/main-carousel.component';
@@ -23,24 +25,6 @@ import { FooterComponent } from './component/footer/footer.component';
 import { TournamentHomeComponent } from './pages/tournament-home/tournament-home.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBdUV4H2KutfYcW6sFNKNNWLGGdH-BPibo",
-  authDomain: "football-tournament-planner.firebaseapp.com",
-  projectId: "football-tournament-planner",
-  storageBucket: "football-tournament-planner.appspot.com",
-  messagingSenderId: "795004126439",
-  appId: "1:795004126439:web:bc65487e3142bf1b714e42",
-  measurementId: "G-RG8S4ZDF7D"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 @NgModule({
   declarations: [
@@ -64,9 +48,13 @@ const analytics = getAnalytics(app);
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, //
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'football-tournament-planner',
+    appId: '1:795004126439:web:bc65487e3142bf1b714e42',
+    storageBucket: 'football-tournament-planner.appspot.com',
+    apiKey: 'AIzaSyBdUV4H2KutfYcW6sFNKNNWLGGdH-BPibo',
+    authDomain: 'football-tournament-planner.firebaseapp.com',
+    messagingSenderId: '795004126439',
+    measurementId: 'G-RG8S4ZDF7D',
+  },
   production: true
 };
